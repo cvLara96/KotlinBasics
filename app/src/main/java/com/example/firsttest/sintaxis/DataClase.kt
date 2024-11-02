d614f66da35e49f03af49f43cbf2125faf98d7d1
Carlos Ventas Lara <carlosventas_1996@hotmail.com>
2024-11-02 22:45:56 +0100

    Modificacion de DataClass

--- app/src/main/java/com/example/firsttest/sintaxis/DataClase.kt
+++ app/src/main/java/com/example/firsttest/sintaxis/DataClase.kt
@@ -2,11 +2,11 @@ package com.example.firsttest.sintaxis
 
 fun main(){
 
-    //val persona1:Persona = Persona("Carlos", "Ventas")
-    //val persona2:Persona= Persona("Pablo", "Palomino")
+    val persona1:Persona = Persona("Carlos", "Ventas", CivilStatus.Comprometed)
+    val persona2:Persona= Persona("Pablo", "Palomino", CivilStatus.Married)
 
     //Podemos hacer lo mismo que con las clases normales, pero es mas sencilla
-    //println(persona1.name)
-    //println(persona2.name)
+    println(persona1.name)
+    println(persona2.name)
 
 }
